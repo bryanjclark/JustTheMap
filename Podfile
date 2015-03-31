e5d37086b09a124132c3f424279879794125cd66
Bryan Clark <bryclark@starbucks.com>
2015-03-31 14:35:18 -0700

    Adding Pods
    Tweaks are on the way!

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '7.0'
+
+pod 'HexColors', '~> 2.2'
+pod 'Tweaks', '~> 1.1'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// HexColors
+#define COCOAPODS_POD_AVAILABLE_HexColors
+#define COCOAPODS_VERSION_MAJOR_HexColors 2
+#define COCOAPODS_VERSION_MINOR_HexColors 2
+#define COCOAPODS_VERSION_PATCH_HexColors 1
+
+// Tweaks
+#define COCOAPODS_POD_AVAILABLE_Tweaks
+#define COCOAPODS_VERSION_MAJOR_Tweaks 1
+#define COCOAPODS_VERSION_MINOR_Tweaks 1
+#define COCOAPODS_VERSION_PATCH_Tweaks 0
+
